b8c0e9db28c7c8b8fbecf24ea2cb77e9bbeafb58
manas <manasbhondve@gmail.com>
2025-04-15 22:04:13 +0530

    Initial commit of E-commerce MERN project

---
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react'
+import './ListProduct.css'
+import cross_icon from '../../assets/Admin_Assets/cross_icon.png'
+
+const ListProduct = () => {
+    const [allproducts,setAllProducts]=useState([]);
+    const fetchInfo=async()=>{
+        await fetch('http://localhost:4000/allproducts').then((res)=>res.json()).then((data)=>{setAllProducts(data)});
+    }
+    useEffect(()=>{
+        fetchInfo();
+    },[])
+    const remove_product=async(id)=>{
+        await fetch('http://localhost:4000/removeproduct',{
+            method:'POST',
+            headers:{
+                Accept:'application/json',
+                'Content-Type':'application/json',
+            },
+            body:JSON.stringify({id:id})
+        })
+        await fetchInfo();
+    }
+  return (
+    <div className='list-product'>
+        <h1>All Products List</h1>
+        <div className="listproduct-format-main">
+            <p>Products</p>
+            <p>Title</p>
+            <p>Old Price</p>
+            <p>New Price</p>
+            <p>Category</p>
+            <p>Remove</p>
+        </div>
+        <div className="listproduct-allproducts">
+            <hr/>
+            {allproducts.map((products,index)=>{
+                return <><div key={index} className="listproduct-format-main listproduct-format">
+                <img src={products.image} className="listproduct-product-icon" />
+                <p>{products.name}</p>
+                <p>${products.old_price}</p>
+                <p>${products.new_price}</p>
+                <p>{products.category}</p>
+                <img onClick={()=>{remove_product(products.id)}} src={cross_icon} className='listproduct-remove-icon'/>
+            </div>
+            <hr/></>
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default ListProduct
