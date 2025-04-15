3bd6273dace4949f9f813ea9f555f307a7da512b
manas <manasbhondve@gmail.com>
2025-04-15 22:14:34 +0530

    Fixed nested frontend folder issue

---
+++ Frontend/src/Components/CartItems/CartItems.jsx
@@ -0,0 +1,70 @@
+import React, { useContext } from 'react'
+import './CartItems.css'
+import { ShopContext } from '../../Context/ShopContext'
+
+const CartItems = () => {
+  const {all_product,cartItems,removeFromCart,getTotalCartAmount}=useContext(ShopContext);
+  const giveAlert=()=>{
+    alert("Your order has been placed !");
+  }
+  return (
+    <div className='cartitems'>
+        <div className="cartitems-format-main">
+            <p>Products</p>
+            <p>Title</p>
+            <p>Price</p>
+            <p>Quantity</p>
+            <p>Total</p>
+            <p>Remove</p>
+        </div>
+        <hr/>
+        {all_product.map((e)=>{
+            if(cartItems[e.id]>0){
+                return <div>
+                <div className="cartitems-format cartitems-format-main">
+                    <img className='carticon-product-icon' src={e.image}/>
+                    <p>{e.name}</p>
+                    <p>${e.new_price}</p>
+                    <button className='cartitems-quantity'>{cartItems[e.id]}</button>
+                    <p>${e.new_price*cartItems[e.id]}</p>
+                    <button className='removebutton' onClick={()=>{removeFromCart(e.id)}}>Remove</button>
+                </div>
+                <hr/>
+            </div>
+            }
+            return null;
+        })}
+        <div className="cartitems-down">
+            <div className="cartitems-total">
+                <h1>Cart Totals</h1>
+                <div>
+                    <div className="cartitems-total-item">
+                        <p>Subtotal</p>
+                        <p>${getTotalCartAmount()}</p>
+                    </div>
+                    <hr/>
+                    <div className="cartitems-total-item">
+                        <p>Shipping Fee</p>
+                        <p>Free</p>
+                    </div>
+                    <hr/>
+                    <div className="cartitems-total-item">
+                        <h3>Total</h3>
+                        <h3>${getTotalCartAmount()}</h3>
+                    </div>
+                </div>
+                <button onClick={giveAlert}>PROCEED TO CHECKOUT</button>
+            </div>
+            <div className="cartitems-promocode">
+                <p>If you have a prome code , Enter it hear</p>
+                <div className="cartitems-promobox">
+                    <input type='text' placeholder='promo code'/>
+                    <button>Submit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CartItems
---
+++ Frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>
+            Discover a world of convenience, quality, and choice with our wide range of products tailored to meet your every need. From trendy fashion and cutting-edge gadgets to home essentials and lifestyle must-haves, we bring you the best at unbeatable prices.Enjoy seamless shopping with user-friendly navigation, secure payment options, and fast delivery right to your doorstep.Whether you're upgrading your wardrobe, finding the perfect gift, or enhancing your home, Shopper is here to make your shopping experience delightful and hassle-free. Shop with confidence and let us bring a world of possibilities to your fingertips! 
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
